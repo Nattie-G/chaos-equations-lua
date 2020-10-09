1e738857537a5e3f9ff6bac8aa9d4b9777fe099f
Nathan L. Gill <nathan.gill@tuta.io>
2020-10-10 03:34:14 +1100

    added config options, put controls and config info in README

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # chaos-equations-lua
 simple clone of hackerpoet's chaos equations
 https://github.com/HackerPoet/Chaos-Equations written in lua and love2d https://love2d.org/
-the entire program is about 300 lines of code
+the entire program is only about 300 lines of code
+
+meant to be hacked on
+before running open up conf.lua and set the window size to something reasonable for your screen
+some options can be set in the options = {} section
+
+## controls
+
+z (zoom) fast-forward: go faster (mulitplier configurable in options)
+x (slow) slow-forward: go slower (mulitplier configurable in options)
+c (rewind)    rewind: hold c down to play the visualisation backwards
+r (random)  equation: generate a new random equation and reset t
+t (time)  reset time: reset t to -1.2
+mouse wheel  scale
+
+## options
+trails:     true/false  whether to display a trail behind each point
+colour:     true/false  when true generate a unique hue for each point. Otherwise use white
+number:     integer     how many points to calculate and display. (recommended < 1000)
+step:       float       multiplier to delta. higher values increase play speed (default 1 / 20)
+zoom_speed: float       multiplier to t while z is pressed (default 10)
+slow_speed: float       multiplier to t while x is pressed (default 0.2)
+point_size: float       radius to draw the point
+show_text:  true/false  display debug text
+
+interest_scaling: true/false attempt to optimise play speed based by estimating how intereseting the current configuration on screen is
+dynamic_scaling:  true/false optimise play speed based on the fastest moving point using a rolling multiplier
--- main.lua
+++ main.lua
@@ -1,14 +1,11 @@
 function love.load()
   WIDTH, HEIGHT = love.graphics.getDimensions()
   SCALE = 400
-  STEP = 1 / 10
   t = -1.2
   my_func = make_function()
   history = {}
-  on_screen = {}
+  --on_screen = {}
   tally = 0
-  n_points = 300
-  constant_time = false
   canv1 = love.graphics.newCanvas()
   canvbg = love.graphics.newCanvas()
   love.graphics.setCanvas(canvbg)
@@ -16,6 +13,14 @@ function love.load()
   love.graphics.setCanvas()
   PAUSED = false
   dynamic_speed_factor = 1.0
+  -- you can edit these!
+  options = { trails      = true, colour      = true,
+              number      = 200 , step        = 1/20,
+              zoom_speed  = 10  , slow_speed  = 0.2 ,
+              point_size  = 2   , show_text   = true,
+              interest_scaling  = true,
+              dynamic_scaling   = true
+            }
 end
 
 function change_function()
@@ -35,25 +40,27 @@ function love.keypressed(key)
   end
 end
 
+-- resizing doesn't quite work properly
 function love.resize(w, h)
   WIDTH, HEIGHT = w, h
 end
 
+-- adjust the scale of the graph with the mouse wheel
 function love.wheelmoved(x, y)
-  SCALE = SCALE + (y * 50)
+  SCALE = math.max(SCALE + (y * 50), 0)
 end
 
 
 function love.update(dt)
+  love.timer.sleep(1/120)
   if PAUSED then
     return
   end
-  love.timer.sleep(1/120)
-  table.insert(history, 1, solve(my_func, n_points))
+  table.insert(history, 1, solve(my_func, options.number))
   if #history > 30 then table.remove(history) end
 
-  tally = 0
-  on_screen = {}
+  local tally = 0
+  local on_screen = {}
   for index, point in ipairs(history[1]) do
     if is_on_screen(point) then
       tally = tally + 1
@@ -61,34 +68,32 @@ function love.update(dt)
     end
   end
 
-  if constant_time then 
-    interest = 1
-  else
-    interest = interest2(history)
-  end
-
-  adjust_dynamic_speed()
+  interest = interest2(history, on_screen, tally)
+  adjust_dynamic_speed(on_screen, tally)
   local speed_mult = interest * dynamic_speed_factor
 
-  local next_t = t + (dt * STEP * dynamic_speed_factor)
+  local key_adjustment = 1
   if love.keyboard.isDown('z') then
-    t = t + (10 * dt * STEP * dynamic_speed_factor)
-  elseif love.keyboard.isDown('x') then
-    t = t + (0.02 * dt * STEP * dynamic_speed_factor)
-  elseif love.keyboard.isDown('c') then
-    t = t - (dt * STEP)
-  else 
-    t = t + (dt * STEP * dynamic_speed_factor)
+    key_adjustment = key_adjustment * options.zoom_speed
+  end
+  if love.keyboard.isDown('x') then
+    key_adjustment = key_adjustment * options.slow_speed
+  end
+  if love.keyboard.isDown('c') then
+    key_adjustment = -1 * key_adjustment
   end
 
+  t = t + (dt * options.step * dynamic_speed_factor * key_adjustment)
+
   if (t > 1.2) and interest >= 1 then
-    t = -1.2
-    my_func = make_function()
+    change_function()
   end
-  -- do additional testing for new dots "blinking in" here
 end
 
 function draw_text()
+  if not options.show_text then
+    return
+  end
   love.graphics.setColor(1, 1, 1)
   local seperator = string.find(my_func, ',')
   local equation_1 = string.sub(my_func, 9, seperator)
@@ -105,7 +110,7 @@ function draw_text()
   love.graphics.print("interest " .. interest_val, 60, 300, 0, 4, 4)
   love.graphics.print("dynamic_speed_factor " .. speed_factor, 60, 365, 0, 4, 4)
 
-  love.graphics.print("SCALE" .. tostring(SCALE), 700, 180, 0, 4, 4)
+  love.graphics.print("scale " .. tostring(SCALE) .. "x", 500, 180, 0, 4, 4)
 end
 
 
@@ -116,33 +121,39 @@ function love.draw()
     return
   end
 
-  love.graphics.setBlendMode("alpha", "alphamultiply")
   love.graphics.setCanvas(canv1)
-  love.graphics.draw(canvbg)
-  love.graphics.setBlendMode("add", "premultiplied")
+  love.graphics.setBlendMode("alpha", "alphamultiply")
+
+  if options.trails then
+    love.graphics.draw(canvbg)
+    love.graphics.setBlendMode("add", "premultiplied")
+  else
+    love.graphics.clear(0, 0, 0)
+  end
 
   local length = #history
   local limit = #history[length]
 
+  love.graphics.setColor(1, 1, 1)
   for i=1, limit do
     if is_on_screen(history[1][i]) then
       local p = plot_point(history[1][i])
-      love.graphics.setColor(unique_colour(i))
-      love.graphics.circle("fill", p[1], p[2], 2.0)
+      if options.colour then
+        love.graphics.setColor(unique_colour(i))
+      end
+      love.graphics.circle("fill", p[1], p[2], options.point_size)
     end
   end
 
   love.graphics.setColor(1, 1, 1)
-
   love.graphics.setBlendMode("alpha", "alphamultiply")
-  --love.graphics.setBlendMode("lighten", "premultiplied")
   love.graphics.setCanvas() -- send focus to the screen
   love.graphics.draw(canv1)
-  --
-  love.graphics.setBlendMode("alpha", "alphamultiply")
   draw_text()
 end
 
+-- helper function for solve(f, n) uses globals so lua's load()
+-- can read our function instead of having to parse it manually
 function apply_f(f, q)
   x, y = q[1], q[2]
   local p = load(f)()
@@ -150,6 +161,8 @@ function apply_f(f, q)
   return p
 end
 
+-- this function was adapted from 
+-- https://gist.github.com/jnez71/7105e3c2410fda1bef500ff8801a710f
 function solve(f, n)
   local q = {t, t}
   local Q = {}
@@ -178,46 +191,55 @@ function remap(i, low1, high1, low2, high2)
 end
 
 
-function interest2(history)
-  if #history < 2 then
+function interest2(history, on_screen, tally)
+--[[
+the second iteration of my "interest" function attempts to optimize
+play speed by evaluating how interesting the current state is.
+currently, it relies on two metrics and maps them to multipliers
+between 0.5 and 2.0. the product of the multiplier is used in the
+update function to multiply delta time. the current factors used are
+the fraction of points on the screen and the area of screen covered
+--]]
+  if not options.interest_scaling or (#history < 2) then
     return 1
   end
 
-  count_multiplier = remap(tally, 0, n_points, 1.5, 0.5)
-  --print("tally multiplier", count_multiplier)
+  count_multiplier = remap(tally, 0, options.number, 1.5, 0.5)
 
   local min_x, min_y = WIDTH, HEIGHT
   local max_x, max_y = 0, 0
   local x_accum, y_accum = 0, 0
   for i, p in pairs(on_screen) do
-    local screen_point = plot_point(p)
-    x_accum = x_accum + screen_point[1]
-    y_accum = y_accum + screen_point[2]
-    min_x = (min_x < screen_point[1]) and min_x or screen_point[1]
-    min_y = (min_y < screen_point[2]) and min_y or screen_point[2]
-    max_x = (max_x > screen_point[1]) and max_x or screen_point[1]
-    max_y = (max_y > screen_point[2]) and max_y or screen_point[2]
+    local screen_coords = plot_point(p)
+    x_accum = x_accum + screen_coords[1]
+    y_accum = y_accum + screen_coords[2]
+    min_x = (min_x < screen_coords[1]) and min_x or screen_coords[1]
+    min_y = (min_y < screen_coords[2]) and min_y or screen_coords[2]
+    max_x = (max_x > screen_coords[1]) and max_x or screen_coords[1]
+    max_y = (max_y > screen_coords[2]) and max_y or screen_coords[2]
   end
-  local mid_point = {x_accum / #on_screen, y_accum / #on_screen}
-  local bbox = {min_x, min_y, max_x, max_y}
   local dx = math.max(max_x - min_x, 1)
   local dy = math.max(max_y - min_y, 1)
 
+  -- using cube roots to compare screen area diminishes
+  -- the difference in order of magnitude for typical comparisons
   local area_root = (dx * dy)^(1/3)
   local screen_root = (WIDTH * HEIGHT)^(1/3)
   local area_factor = area_root / screen_root
   local adjusted_area_factor = remap(area_factor, 0, 1, 2.0, 0.5)
 
-  --print("area_factor", string.sub(tostring(area_factor), 1, 4))
-  --print("adjusted_area_factor", string.sub(tostring(adjusted_area_factor), 1, 4))
-
-
   return count_multiplier * adjusted_area_factor
 end
 
-function adjust_dynamic_speed()
-  if #history < 2 then
-    return
+function adjust_dynamic_speed(on_screen, tally)
+--[[
+this function could possibly replace the interest function approach
+but currently operates alongside it. speed is adjusted in a rolling
+fashion by using a multiplier to adjust away from extremes of low and
+high speed. it actually works quite well
+--]]
+  if not options.dynamic_scaling or (#history < 2) then
+    return 1
   end
   biggest_dist = #on_screen == 0 and 1 or 0
   --smallest_dist = 1000
@@ -239,38 +261,10 @@ function adjust_dynamic_speed()
   --print("biggest_dist", biggest_dist)
 end
 
-function plot_point(q)
+function plot_point(q) -- convert graph coords to screen coords
   return {(q[1] * SCALE) + (WIDTH / 2), (q[2] * SCALE) + (HEIGHT / 2)}
 end
 
-function new_term(variable, coefficients)
-
-  function d3_string(c) -- int, string -> string OR nil
-  -- sign a or zero a term with equal probability
-    local ss = {'-' .. c, 0, c}
-    return ss[love.math.random(1, 3)]
-  end
-
-  function d4_string(c) -- int, string, table -> string
-    -- add coefficients to the term
-    -- 1 - coefficient 1 & 2
-    -- 2 - coefficient 1 of 2
-    -- 3 - coefficient 2 of 2
-    -- 4 - no coefficient
-    local cc = {c == 'x' and 'y' or 'x',
-                c == 't' and 'y' or 't'
-      }
-    local ss = {
-        cc[1] .. '*' .. cc[2] .. '*' .. c,
-        cc[1] .. '*' .. c,
-        cc[2] .. '*' .. c,
-        c
-      }
-    return ss[love.math.random(1, 4)]
-  end
-
-  return d3_string(d4_string(variable, coefficients))
-end
 
 
 function unique_colour(i) -- i <= 1000
@@ -296,11 +290,30 @@ function HSV(h, s, v)
   end return {(r+m), (g+m), (b+m)}
 end
 
+
+
 function make_function()
-  local z = '0'
+  -- helper function
+  function new_term(term, coefficients)
+    local root = string.sub(tostring(term), 1, 1)
+    local coefs = { root == 'x' and 'y' or 'x',
+                    root == 't' and 'y' or 't'
+      }
+    local append_coefs = {
+        coefs[1] .. '*' .. coefs[2] .. '*' .. term, -- coef 1 & 2
+        coefs[1] .. '*' .. term,                    -- coef 1
+        coefs[2] .. '*' .. term,                    -- coef 2
+        term                                        -- no coef
+      }
+
+    local full_term = append_coefs[love.math.random(1, 4)]
+    local sign_or_zero = {'-' .. full_term, '0' , full_term}
+    local signed_full_term = sign_or_zero[love.math.random(1, 3)]
+    return signed_full_term
+  end
+
   local rules = {}
   local coefficients = {'t', 'x', 'y'}
-  local tx, ty, xy = {'t', 'x'}, {'t', 'y'}, {'x', 'y'}
   for i = 1, 2 do
     -- x^2 term
     local xx = new_term('x^2')
@@ -315,7 +328,7 @@ function make_function()
     -- t term
     local t = new_term('t')
     rules[i] =  (xx .. ' + ' .. yy .. ' + ' .. tt
-      .. ' + ' .. x  .. ' + ' .. y  .. ' + ' .. tt)
+      .. ' + ' .. x  .. ' + ' .. y  .. ' + ' .. t)
   end
   local func = 'return {' .. rules[1] .. ', ' .. rules[2] .. '}'
   --local func = 'return {custom x function, custom y function}'
